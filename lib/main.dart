700aa11061f853d52054a22ec2c6f912158e9623
moustafa abdel-ghany <moustafaabdelghany>
2020-12-14 05:20:03 +0200

    Mi_Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,84 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/mater\ial.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/down.png'),
+              ),
+              Text(
+                'Moustafa Abdel-ghany',
+                style: TextStyle(
+                  fontFamily: "Pacifico_Regular",
+                  fontSize: 30.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: "Source Sans Pro",
+                  color: Colors.teal[100],
+                  fontSize: 20.0,
+                  letterSpacing: 1.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 150.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.teal.shade50,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title:Text(
+                    '+201146703048',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source San Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'moustafa.1001007@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.teal.shade900,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ))),
+    );
+  }
 }
